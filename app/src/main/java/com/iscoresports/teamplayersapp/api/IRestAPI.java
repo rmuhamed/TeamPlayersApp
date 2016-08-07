5dc052657ba68a7f21d13ac12bc038eeafbc0e2e
rmuhamed <ricardo.muhamed@gmail.com>
2016-08-07 10:10:59 -0300

    Kick off - Data rendering

--- app/src/main/java/com/iscoresports/teamplayersapp/api/IRestAPI.java
+++ app/src/main/java/com/iscoresports/teamplayersapp/api/IRestAPI.java
@@ -4,6 +4,7 @@ import com.iscoresports.teamplayersapp.model.Team;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by rmuhamed on domingo.
@@ -11,4 +12,8 @@ import retrofit2.http.GET;
 public interface IRestAPI {
     @GET("team.json")
     Call<Team> getTeam();
+
+    @GET("tapped.php?teamid={teamid}&playerid={playerid}&firstname={PlayerFirstName}&lastname={PlayerLastName}")
+    String getPlayerInfo();
+
 }
--- app/src/main/java/com/iscoresports/teamplayersapp/api/RestAPIImpl.java
+++ app/src/main/java/com/iscoresports/teamplayersapp/api/RestAPIImpl.java
@@ -12,7 +12,7 @@ import retrofit2.http.HTTP;
 /**
  * Created by rmuhamed on domingo.
  */
-public class RestAPIImpl implements Callback<Team> {
+public class RestAPIImpl {
     private final APIResultListener listener;
 
     public RestAPIImpl(APIResultListener<Team> listener) {
@@ -24,21 +24,26 @@ public class RestAPIImpl implements Callback<Team> {
                 RetrofitConfiguration.getInstance().create(IRestAPI.class);
 
         Call<Team> call = restAPI.getTeam();
-        call.enqueue(this);
+        call.enqueue(new Callback<Team>() {
+            @Override
+            public void onResponse(Call<Team> call, Response<Team> response) {
+                int statusCode = response.code();
+                if (statusCode == Constants.HTTP_OK) {
+                    RestAPIImpl.this.listener.onSuccess(response.body());
+                } else {
+                    RestAPIImpl.this.listener.onError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Team> call, Throwable t) {
+                RestAPIImpl.this.listener.onError();
+            }
+        });
     }
 
-    @Override
-    public void onResponse(Call<Team> call, Response<Team> response) {
-        int statusCode = response.code();
-        if (statusCode == Constants.HTTP_OK) {
-           this.listener.onSuccess(response.body());
-        } else {
-            this.listener.onError();
-        }
+    public void getPlayerInformation() {
+
     }
 
-    @Override
-    public void onFailure(Call<Team> call, Throwable t) {
-        this.listener.onError();
-    }
 }
